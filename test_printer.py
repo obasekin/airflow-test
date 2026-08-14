fb1482ef1d0d8b4106b990dbb2d9242872c7723e
Ömer Başekin <omerbasekin@MacBook-Air.local>
2026-08-14 13:42:22 +0300

    add 2nd test

--- test_printer.py
+++ test_printer.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.operators.dummy import DummyOperator
+from airflow.operators.empty import EmptyOperator
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from kubernetes.client import models as k8s
 
