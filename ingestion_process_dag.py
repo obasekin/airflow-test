d7b7ba7380659becb7a1336790dd68e48c4fcb05
Omer Basekin <obasekin@arcanor.com>
2026-09-16 14:14:36 +0300

    remove beltest

--- ingestion_process_dag.py
+++ ingestion_process_dag.py
@@ -49,7 +49,7 @@ def ingestion_process_workflow():
         files = conf.get("files")
         ingestion_spec_path = conf.get("ingestion_spec_path")
 
-        if country not in ("BEL", "NLD", "TURv2", "TUR", "BELtest"):
+        if country not in ("BEL", "NLD", "TURv2", "TUR"):
             raise ValueError(
                 "country must be BEL, NLD, TURv2, TUR or BELtest"
             )
