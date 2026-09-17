970f78047528c0b3ba4f3f088639d3abc2e6744a
Omer Basekin <obasekin@arcanor.com>
2026-09-17 10:56:01 +0300

    add runner notebook

--- existing_notebook_runner_dag.py
+++ existing_notebook_runner_dag.py
@@ -25,7 +25,7 @@ def existing_notebook_runner_workflow():
     def run_notebook() -> dict:
         # Notebook dosyasinin JupyterHub ana dizinindeki (ya da ilgili klasordeki) yolu.
         # Ornegin: "analysis/my_daily_analysis.ipynb"
-        notebook_path = "airflow_notebook_example.ipynb"
+        notebook_path = "airflow_notebook_1789579027.ipynb"
         
         # run_existing_notebook fonksiyonunu cagiriyoruz
         result = run_existing_notebook(
